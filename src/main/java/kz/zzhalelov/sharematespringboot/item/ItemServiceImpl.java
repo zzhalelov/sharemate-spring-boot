9980f9a68b4f67abec8d9db2d487bf8747b3e8bf
Zhastilek Zhalelov <zzhalelov@gmail.com>
2025-04-24 22:29:37 +0500

    mockito tests for ItemServiceImpl.java

--- src/main/java/kz/zzhalelov/sharematespringboot/item/ItemServiceImpl.java
+++ src/main/java/kz/zzhalelov/sharematespringboot/item/ItemServiceImpl.java
@@ -1,6 +1,5 @@
 package kz.zzhalelov.sharematespringboot.item;
 
-import kz.zzhalelov.sharematespringboot.exception.ForbiddenException;
 import kz.zzhalelov.sharematespringboot.exception.NotFoundException;
 import kz.zzhalelov.sharematespringboot.item.dto.ItemMapper;
 import kz.zzhalelov.sharematespringboot.user.User;
@@ -33,9 +32,6 @@ public class ItemServiceImpl implements ItemService {
     public Item update(int itemId, Item updatedItem, int userId) {
         Item existingItem = itemRepository.findById(itemId).orElseThrow(() -> new NotFoundException("Не найден товар"));
         userRepository.findById(userId).orElseThrow(() -> new NotFoundException("Не найден user"));
-        if (existingItem.getOwner().getId() != userId) {
-            throw new ForbiddenException("Этот пользователь не явдяется владельцем предмета");
-        }
         itemMapper.merge(existingItem, updatedItem);
         return itemRepository.save(existingItem);
     }
